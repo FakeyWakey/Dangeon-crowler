0633ed25e755d905219d257719877d0eeea271ab
AlanDzi <alan.gabryelczyk22@gmail.com>
2025-06-22 16:26:28 +0200

    Inventory#4 sm

--- Assets/Scripts/UI's/TabInventory.cs
+++ Assets/Scripts/UI's/TabInventory.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class InventoryToggle : MonoBehaviour
 {
     public GameObject inventoryPanel; // przypisujesz tu panel z Canvy
-
+    public InventoryManager inventoryManager;   
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             bool isActive = inventoryPanel.activeSelf;
             inventoryPanel.SetActive(!isActive);
+            inventoryManager.ListItems();
         }
     }
 
