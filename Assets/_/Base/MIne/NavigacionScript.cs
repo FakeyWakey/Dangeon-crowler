6d53c65ad3e6988cc53a42c580812c6020406b4b
FakeyWakey <pgraczkowska@edu.cdv.pl>
2025-06-22 16:42:16 +0200

    22.06

--- Assets/_/Base/MIne/NavigacionScript.cs
+++ Assets/_/Base/MIne/NavigacionScript.cs
@@ -8,16 +8,27 @@ public class NavigacionScript : MonoBehaviour
 {
     public Transform enemy;
     private NavMeshAgent agent;
-
+    public List<Transform> PatrolPoints = new List<Transform>();
+    public int PatrolPointCounter;
     
     void Start()
     {
+        
         agent = GetComponent<NavMeshAgent>();
     }
 
    
     void Update()
     {
-        agent.destination = enemy.position;
+        agent.destination = PatrolPoints[PatrolPointCounter].position;
+        if (Vector3.Distance(transform.position, agent.destination)<1f ) //jeœli jest 0.1 away from the point zalicza punkt
+        {
+            PatrolPointCounter++;
+            if (PatrolPointCounter >= PatrolPoints.Count)
+            { 
+                PatrolPointCounter = 0;
+            }
+        } 
+
     }
 }
